df57c017db8cee8b9e74d0ae6f3cf4a5e06fc181
Chantal <chantalschot@outlook.com>
2020-02-06 09:32:00 +0100

    Initial commit

---
+++ src/main/java/nl/capgemini/setracknarnia/NarniaApplication.java
@@ -0,0 +1,13 @@
+package nl.capgemini.setracknarnia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/capgemini/setracknarnia/NarniaApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.capgemini.setracknarnia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
